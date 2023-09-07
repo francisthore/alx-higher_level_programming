079cd33c626301f02041d95abbcf30c35e41ae17
Francis Thore <thorefrancis@gmail.com>
2023-09-07 15:15:31 -0700

    Ohh I had to create a main

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
-import add_0
+def main():
+    from add_0 import add
 
-a = 1
-b = 2
+    a = 1
+    b = 2
 
-print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
+if __name__ == '__main__':
+    main()
