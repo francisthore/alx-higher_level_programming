b3a7671cded9fd36201de3b6d6b504215714b554
Francis Thore <thorefrancis@gmail.com>
2023-09-11 15:18:03 -0700

    Shallow cpy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/bash/python3
+
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    list_cpy = my_list[:]
+    list_cpy[idx] = element
+    return list_cpy
