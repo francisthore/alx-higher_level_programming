9a24de5bb9347e185032c24466d3e842c97c82de
Francis Thore <thorefrancis@gmail.com>
2023-09-11 15:00:00 -0700

    Checks if lists is empty now

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    length = len(my_list) - 1
-    i = length - 1
-    idx = i - length
-
+    idx = len(my_list) - 1
+    
+    if idx < 0:
+        return
     for elem in my_list:
         print("{:d}".format(my_list[idx]))
-        i -= 1
-        idx = i - length
+        idx -= 1
