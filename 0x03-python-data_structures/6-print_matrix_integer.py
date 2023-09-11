6e63b158b0cae8648f129ba3ec2eb2b4c63612bb
Francis Thore <thorefrancis@gmail.com>
2023-09-11 16:22:32 -0700

    Checking if matrix is empty

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return
     for i in range(0, len(matrix)):
         for j in range(0, len(matrix[i])):
             print("{:d}".format(matrix[i][j]), sep=" ", end="")
