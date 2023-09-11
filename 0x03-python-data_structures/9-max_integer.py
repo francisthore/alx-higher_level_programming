8b78be5bb1d55b0f596be48f9719dedf857281a5
Francis Thore <thorefrancis@gmail.com>
2023-09-11 16:41:05 -0700

    So my init value was flowed

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,8 +3,8 @@
 def max_integer(my_list=[]):
     if not my_list:
         return
-    max_int = 0
-    for i in range(0, len(my_list)):
+    max_int = my_list[0]
+    for i in range(1, len(my_list)):
         if my_list[i] >= max_int:
             max_int = my_list[i]
     return max_int
