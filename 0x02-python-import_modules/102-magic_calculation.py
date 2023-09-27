cca0b28fbc7b35d7a33a944a7ccf4feb18e54934
Francis Thore <thorefrancis@gmail.com>
2023-09-27 21:03:07 +0200

    lets see

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,15 @@
+#!/user/bin/python3
+
+def magic_calculation(a, b):
+    add, sub = None, None
+    
+    if a < b:
+        add = __import__('magic_calculation_102').add
+        c = add(a, b)
+        
+        for i in range(4, 7):
+            c = add(c, i)
+        
+        return c
+    
+    return __import__('magic_calculation_102').sub(a, b)
