64754c8fd373a6a875c83f16d6f52eda4ecf79ea
Francis Thore <thorefrancis@gmail.com>
2023-09-27 21:11:11 +0200

    pycode fam

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,14 +2,11 @@
 
 def magic_calculation(a, b):
     add, sub = None, None
-    
+
     if a < b:
         add = __import__('magic_calculation_102').add
         c = add(a, b)
-        
         for i in range(4, 7):
             c = add(c, i)
-        
         return c
-    
     return __import__('magic_calculation_102').sub(a, b)
