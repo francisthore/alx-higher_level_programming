ce9d08ec933ddfc28c3d422d92f7d8bf08589a20
Francis Thore <thorefrancis@gmail.com>
2023-11-16 14:36:11 -0800

    mmmh

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 
 class Square:
-    """This is an empty class that defines a square
-
-    This is my first ever python class with ALX and this is also
-    the first docstring I am writing in Python.
-
-    """
+    """This is an empty class that defines a square"""
     pass
