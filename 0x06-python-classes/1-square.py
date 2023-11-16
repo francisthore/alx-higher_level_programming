109e541f7dc52d13a3d52a3fd480408e00c5a45b
Francis Thore <thorefrancis@gmail.com>
2023-11-16 14:24:33 -0800

    Python OOP first commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class Square:
+    """This is an empty class that defines a square
+
+    This is my first ever python class with ALX and this is also
+    the first docstring I am writing in Python.
+
+    """
+    pass
