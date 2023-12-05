5a63a377e154168bc6c9d1763af8a2a448206147
Francis Thore <thorefrancis@gmail.com>
2023-12-05 19:17:49 +0200

    Square init

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This is a module that defines an squares
+"""
+
+
+class Square:
+    """This is a class that defines a square"""
+
+    def __init__(self, size):
+        """This is a function that initializes a sqaure object
+
+        Args:
+                self: the object itself
+                size: size of one side of a square
+        """
+
+        self._size = size
