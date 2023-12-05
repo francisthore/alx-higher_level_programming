1ebfcca0d9e26b331542831b7d5418752df4de72
Francis Thore <thorefrancis@gmail.com>
2023-12-05 18:47:23 +0200

    spacing

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """
-   This is a module that defines an squares
-
-
+This is a module that defines an squares
 """
 
 
 class Square:
-   """This is an empty class that defines a square"""
-   pass
+    """This is an empty class that defines a square"""
+    pass
