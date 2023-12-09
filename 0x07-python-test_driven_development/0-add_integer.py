2d060c5d94531b46b524df46b8e59fb4ea5683e5
Francis Thore <thorefrancis@gmail.com>
2023-12-09 17:52:37 +0200

    checks none

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,10 +11,10 @@ def add_integer(a, b=98):
     """This function adds two integers
         Returns sum of the two integers
     """
-    if ((type(a) is not int) and (type(a) is not float)):
+    if (a in None or (type(a) is not int) and (type(a) is not float)):
         raise TypeError("a must be an integer")
 
-    if ((type(b) is not int) and (type(b) is not float)):
+    if (b is None or (type(b) is not int) and (type(b) is not float)):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
