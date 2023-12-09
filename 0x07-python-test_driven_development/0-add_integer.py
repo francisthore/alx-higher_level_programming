aaef65856a384bbcccde73cef592cc3e3f7a9f73
Francis Thore <thorefrancis@gmail.com>
2023-12-09 17:48:55 +0200

    Hello TDD

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""This a module that adds integers
+    It qualifys the values and casts them into ints
+    I am also running tests 
+    I am just putting lines rn idk what to write
+"""
+
+def add_integer(a, b=98):
+    """This function adds two integers
+        Returns sum of the two integers
+    """
+    if ((type(a) is not int) and (type(a) is not float)):
+        raise TypeError("a must be an integer")
+
+    if ((type(b) is not int) and (type(b) is not float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,6 @@
+This is test driven development with python
+Tests are always written before production.
+Afterall a wise man once said 
+    """
+        Never trust a user, always test
+    """ ~ Wise Man
