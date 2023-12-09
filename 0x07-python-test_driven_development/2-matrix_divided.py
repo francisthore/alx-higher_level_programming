ea98a591b90aafe4d5d3f21e6768a6bc5ac1571c
Francis Thore <thorefrancis@gmail.com>
2023-12-09 23:36:04 +0200

    I have more tests in mind

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This is a module that divides a matrix
+    It checks if the list is ints and floats only
+    Tests will be written to check
+    And so help us God
+"""
+
+
+def matrix_divided(matrix, div):
+    """This function divides a matrix
+        Returns a new matrix of answers
+    """
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    for ls in matrix:
+        for i in range(len(ls)):
+            if not isinstance(matrix, list) or not isinstance(ls[i], (float, int)):
+                raise TypeError(
+                  "matrix must be a matrix (list of lists) of integers/floats"
+                )
+    res_final = []
+    for sub_list in matrix:
+        res_tmp = []
+        for idx in range(len(sub_list)):
+            res_tmp.append(round((sub_list[idx] / div), 2))
+        res_final.append(res_tmp)
+    return res_final
