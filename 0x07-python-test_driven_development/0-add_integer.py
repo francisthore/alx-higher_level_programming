a5030588f27648263e683b9876236bf89aed633b
Francis Thore <thorefrancis@gmail.com>
2023-12-09 17:53:53 +0200

    14 & 15 failes

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,10 +11,10 @@ def add_integer(a, b=98):
     """This function adds two integers
         Returns sum of the two integers
     """
-    if (a in None or (type(a) is not int) and (type(a) is not float)):
+    if ((type(a) is not int) and (type(a) is not float)):
         raise TypeError("a must be an integer")
 
-    if (b is None or (type(b) is not int) and (type(b) is not float)):
+    if ((type(b) is not int) and (type(b) is not float)):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
