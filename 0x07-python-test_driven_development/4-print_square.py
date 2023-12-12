80c6e01b5400235bfcb5562261aa4c55ddd73ab5
Francis Thore <thorefrancis@gmail.com>
2023-12-12 21:55:01 +0200

    Square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Prints a square using # signs
+    Has 1 function that handles everything
+    tests will be written separately
+    Lets goooo
+"""
+
+
+def print_square(size):
+    """Prints a square using #
+        tests separated
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    for i in range(size):
+        print("#" * size)
+    
