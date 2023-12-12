f01b610af45ca5ca2917d31c66ac1210c40d1f94
Francis Thore <thorefrancis@gmail.com>
2023-12-12 20:15:26 +0200

    fixing tests

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
-matrix = [[6, 9, 12], [21, 35, 45]]
+matrix = [[6, 9, 12], [21, "35", 45]]
     
     
-print(matrix_divided(matrix, "3"))
+print(matrix_divided(matrix, 3))
 print(matrix)
