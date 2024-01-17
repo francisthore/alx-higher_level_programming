cb6c1ef3f3d9fdd134edb946caa5cb98f4e848d2
Francis Thore <thorefrancis@gmail.com>
2024-01-16 17:12:43 -0800

    AVG: average calc

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,4 @@
+-- remove data enrty meeting
+-- a certain criteria
+DELETE FROM second_table
+WHERE score <= 5;
---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- average function 
+-- to calculate the avg
+SELECT AVG(score) AS average FROM second_table;
