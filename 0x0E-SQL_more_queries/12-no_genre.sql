f453b9fc64ec869d2af4f19a1b4bfb225b81ae7d
Francis Thore <thorefrancis@gmail.com>
2024-01-18 15:45:29 -0800

    filtred joins

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- another join on 
+-- tables that are linked someehow
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
