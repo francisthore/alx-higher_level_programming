4f26b8a64c6121a7890b5f5276fc65e375fd01aa
Francis Thore <thorefrancis@gmail.com>
2024-02-12 23:26:34 +0200

    looping through arr

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// prints content of array
+let i = 0;
+const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+while (i < strings.length) {
+  console.log(strings[i]);
+  i++;
+}
