2f2e4625f4137e87f96831ee94471acf8f2deb26
Francis Thore <thorefrancis@gmail.com>
2024-02-12 23:58:05 +0200

    double for

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// prints x in a sqaure form
+const { argv } = require('process');
+if (argv.length === 2 || !Number(argv[2])) {
+  console.log('Missing size');
+} else if (Number(argv[2]) < 0 || Number(argv[2]) === 0) {
+  // do nex
+} else {
+  let i;
+  let j;
+  for (i = 0; i < Number(argv[2]); i++) {
+    let line = '';
+    for (j = 0; j < Number(argv[2]); j++) {
+      line += 'x';
+    }
+    console.log(line);
+  }
+}
