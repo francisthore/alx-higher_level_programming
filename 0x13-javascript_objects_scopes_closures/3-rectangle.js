aac33528c896885096c4977c2efb0fbce036b805
Francis Thore <thorefrancis@gmail.com>
2024-02-14 17:00:16 +0200

    print []

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// defines rectangle class
+module.export = class Rectangle {
+  constructor (w = 0, h = 0) {
+    if (w <= 0 || h <= 0) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print() {
+    let i;
+    let j;
+    for (i = 0; i < this.height; i++) {
+      let row = '';
+      for (j = 0; j < this.width; j++) {
+        row += 'x';
+      }
+      console.log(row);
+    }
+  }
+};
