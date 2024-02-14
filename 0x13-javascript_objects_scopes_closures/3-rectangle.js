f788f8387eb1341a505c09709b2aacf521eb777b
Francis Thore <thorefrancis@gmail.com>
2024-02-14 17:02:10 +0200

    typo fix: exports

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // defines rectangle class
-module.export = class Rectangle {
+module.exports = class Rectangle {
   constructor (w = 0, h = 0) {
     if (w <= 0 || h <= 0) {
       return;
