83eac7f460e09b1354e6ed4ec1dfb46a08b49e05
Francis Thore <thorefrancis@gmail.com>
2024-02-15 00:00:11 +0200

    103

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,9 +4,10 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-function incr () {
+const incr = function() {
   this.value++;
-}
+};
+
 myObject.incr = incr;
 myObject.incr();
 console.log(myObject);
