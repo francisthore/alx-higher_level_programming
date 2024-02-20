e952767bcc8dc0809ed1d545b3fc5cfcc9f18a56
Francis Thore <thorefrancis@gmail.com>
2024-02-20 13:49:19 +0200

    if name

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,12 +5,13 @@ stuff from a db
 import MySQLdb
 import sys
 
-conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                       port=3306, db=sys.argv[3])
-cursor = conn.cursor()
-cursor.execute('SELECT :id, :name FROM states ORDER BY states.id ASC', {'id':states.id, 'name':states.name})
-res = cursor.fetchall()
-for state in res:
-    print(state)
-cursor.close()
-conn.close()
+if __name__ == '__main__':
+    conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                           port=3306, db=sys.argv[3])
+    cursor = conn.cursor()
+    cursor.execute('SELECT * from states;')
+    res = cursor.fetchall()
+    for state in res:
+        print(state)
+    cursor.close()
+    conn.close()
