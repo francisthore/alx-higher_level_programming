606d645320da43db93fa046609f9d4c33585e5ff
Francis Thore <thorefrancis@gmail.com>
2024-03-01 22:15:33 +0200

    uh

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Sends a post request to
+a server
+"""
+
+if __name__ == "__main__":
+    import urllib
+    import sys
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    values = {'email' : email}
+
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+    print(body.decode('utf-8')
+    
