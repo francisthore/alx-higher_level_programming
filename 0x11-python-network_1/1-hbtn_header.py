5e19de357e617f3fe594cbf7e5fbb5766389b65a
Francis Thore <thorefrancis@gmail.com>
2024-03-01 13:23:27 -0800

    get

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -9,6 +9,4 @@ if __name__ == "__main__":
 
     url = sys.argv[1]
     with urlopen(Request(url)) as response:
-        result = response.getheader('X-Request-Id')
-    if result:
-        print(result)
+        print(dict(response.headers).get("X-Request-Id"))
