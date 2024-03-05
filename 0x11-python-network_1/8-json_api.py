9a903f0ac011775eff9a4c54a06bd4becd224e95
Francis Thore <thorefrancis@gmail.com>
2024-03-05 14:15:05 -0800

    try/exc

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -14,10 +14,12 @@ if __name__ == "__main__":
     url = 'http://0.0.0.0:5000/search_user'
 
     request = requests.post(url, data={'q': letter})
+    try:
+        response = request.json()
 
-    response = request.json()
-
-    if response:
-        print('[{}] {}'.format(response['id'], response['name']))
-    else:
-        print('No result')
+        if response:
+            print('[{}] {}'.format(response['id'], response['name']))
+        else:
+            print('No result')
+    except ValueError:
+        print('Not a valid JSON')
