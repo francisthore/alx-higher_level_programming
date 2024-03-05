4772e21962f5824b37590911e07c08e27d0a907d
Francis Thore <thorefrancis@gmail.com>
2024-03-05 14:11:44 -0800

    api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Sends some data via post as an api
+"""
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    if len(sys.argv) != 1:
+        letter = sys.argv[1]
+    else:
+        letter = ""
+    url = 'http://0.0.0.0:5000/search_user'
+
+    request = requests.post(url, data={'q': letter})
+
+    response = request.json()
+
+    if response:
+        print('[{}] {}'.format(response['id'], response['name']))
+    else:
+        print('No result')
