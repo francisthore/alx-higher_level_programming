cc6fb9520ad14e34c993fdd1fcd04681fc352323
Francis Thore <thorefrancis@gmail.com>
2024-03-19 00:38:22 +0200

    delete

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Prints results of states that contain an
+'a' in  their name
+"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == '__main__':
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        mysql_username, mysql_password, database_name))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query_res = session.query(State).filter(State.name.like('%a%'))
+    for res in query_res:
+        session.delete(res)
+        session.commit()
+    session.close()
