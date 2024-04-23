bd8f953f885fb4ffa1e2576f60939f8a78b815e4
Francis Thore <thorefrancis@gmail.com>
2024-04-23 22:09:44 +0200

    had to export to path

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 // Displays status code of a GET request
 const request = require('request');
-const {argv} = request('process');
+const { argv } = require('process');
 
-url = argv[2];
-request($url, function (error, response, body) {
-    if (error) {
-        console.log(error);
-    }
-    else {
-        console.log('code:', response && response.statusCode);
-    }
+request(argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code:', response && response.statusCode);
+  }
 });
