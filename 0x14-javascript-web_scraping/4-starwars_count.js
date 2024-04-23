ea9c440276eec1f79e70eb6e114b099877b53e1c
Francis Thore <thorefrancis@gmail.com>
2024-04-23 23:17:30 +0200

    filtering

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+// Prints number of movies where a character ID is matched
+const request = require('request');
+const { argv } = require('process');
+const charId = 18;
+const charUrl = 'https://swapi-api.alx-tools.com/api/people/' + charId + '/';
+const url = argv[2];
+
+request({ url, json: true }, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const results = body.results;
+    let i = 0;
+    for (const idx1 in results) {
+      const characters = results[idx1].characters;
+      for (const idx2 in characters) {
+        if (charUrl === characters[idx2]) {
+          i = i + 1;
+        }
+      }
+    }
+    console.log(i);
+  }
+});
