b2ae7686ecf819653f44b304ce243f8cdd7e1223
Francis Thore <thorefrancis@gmail.com>
2024-06-04 22:00:42 +0200

    100

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,11 +3,21 @@
 const request = require('request');
 const { argv } = require('process');
 
-const url = 'https://swapi-api.alx-tools.com/api/films/' + argv[2]
-request ({ url, json: true }, function (error, response, body) {
-	if (error) {
-		console.log(error);
-	} else {
-		console.log(body.characters);
-	}
+const url = 'https://swapi-api.alx-tools.com/api/films/' + argv[2];
+request({ url, json: true }, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = body.characters;
+    for (let i = 0; i < characters.length; i++) {
+      const url = characters[i];
+      request({ url, json: true }, function (error, response, body) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(body.name);
+        }
+      });
+    }
+  }
 });
