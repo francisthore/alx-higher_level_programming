fea76a5d008be1b7e1c5dbd7d980302610fdb293
Francis Thore <thorefrancis@gmail.com>
2024-06-04 20:39:04 +0200

    readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Reads the content of a file using fs
+const fs = require('node:fs');
+const { argv } = require('process');
+
+fs.readFile(argv[2], 'utf-8', (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
