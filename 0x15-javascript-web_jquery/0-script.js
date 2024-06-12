64fcf573876f3fb89dded1e4144f4d386d11a260
Francis Thore <thorefrancis@gmail.com>
2024-06-12 23:24:28 +0200

    first js in web

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+// Updates the text color of the header elem
+document.addEventListener('DOMContentLoaded', function() {
+    const elem = document.querySelector('header');
+    elem.style.color = '#FF0000';
+});
---
+++ 0x15-javascript-web_jquery/README.md
@@ -0,0 +1,1 @@
+JQery project for dynamic stuff
